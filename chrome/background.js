46ae1c8dc1c687385d4e469bcbb4f6200aa35355
unknown <federico.armagno97@gmail.com>
2025-04-19 21:20:22 +0200

    Bug fix and better chrome compatibility

--- chrome/background.js
+++ chrome/background.js
@@ -1,3 +1,33 @@
+const searchInShadowRoot = (selector) => {
+  const queue = [document];
+
+  while (queue.length) {
+    const current = queue.shift();
+    const element = current.querySelector(selector);
+    if (element) {
+      return element;
+    }
+
+    const children = current.querySelectorAll('*');
+    children.forEach(child => {
+      if (child.shadowRoot) {
+        queue.push(child.shadowRoot);
+      }
+    });
+  }
+
+  return null;
+};
+
+const turnOffSwitch = () => {
+  const switchInput = searchInShadowRoot('#hui-translation-switch');
+
+  if (switchInput) {
+    switchInput.setAttribute('aria-checked', 'false');
+    switchInput.removeAttribute('checked');
+  }
+};
+
 const enableAddon = () => {
   chrome.storage.local.get('enabled', (data) => {
     const isEnabled = data.enabled ?? true;
@@ -8,22 +38,39 @@ const enableAddon = () => {
       disableRulesetIds: isEnabled ? [] : ['removeTlParam']
     });
   });
-}
-
-chrome.runtime.onInstalled.addListener(() => enableAddon());
+};
 
 const updateEnabledRuleset = (ruleSetValue) => {
   chrome.declarativeNetRequest.updateEnabledRulesets({
     enableRulesetIds: ruleSetValue ? ['removeTlParam'] : [],
     disableRulesetIds: ruleSetValue ? [] : ['removeTlParam']
   });
-}
+};
 
 chrome.storage.onChanged.addListener((changes, area) => {
   if (!changes.enabled || area !== 'local') {
     return;
   }
-
-  const ruleSetValue = changes.enabled.newValue;
-  updateEnabledRuleset(ruleSetValue);
+  updateEnabledRuleset(changes.enabled.newValue);
 });
+
+chrome.runtime.onInstalled.addListener(() => enableAddon());
+
+const executeTurnOffSwitch = (details) => {
+  if (!details.url.includes("reddit.com")) {
+    return;
+  }
+
+  chrome.storage.local.get('enabled', (data) => {
+    if (!data.enabled) {
+      return;
+    }
+
+    chrome.scripting.executeScript({
+      target: { tabId: details.tabId },
+      func: turnOffSwitch
+    });
+  });
+}
+
+chrome.webNavigation.onCompleted.addListener(details => executeTurnOffSwitch(details), { url: [{ hostContains: 'reddit.com' }] });
