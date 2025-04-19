0549bd41828c87dae5efb977341c948d50293f43
unknown <federico.armagno97@gmail.com>
2025-04-19 19:01:11 +0200

    Reddit is a bitch

---
+++ translationSwitchDisabler.js
@@ -0,0 +1,37 @@
+const searchInShadowRoot = (selector, root = document) => {
+      const queue = [root];
+
+      while (queue.length) {
+            const current = queue.shift();
+            const element = current.querySelector(selector);
+            if (element) {
+                  return element;
+            }
+
+            const children = current.querySelectorAll('*');
+            for (const child of children) {
+                  if (child.shadowRoot) {
+                        queue.push(child.shadowRoot);
+                  }
+            }
+      }
+
+      return null;
+}
+
+const turnOffSwitch = () => {
+      const switchInput = searchInShadowRoot('#hui-translation-switch');
+
+      if (switchInput) {
+            switchInput.setAttribute('aria-checked', 'false');
+            switchInput.removeAttribute('checked');
+            return;
+      }
+      setTimeout(turnOffSwitch, 500);
+}
+
+chrome.storage.local.get("enabled", ({ enabled }) => {
+      if (!enabled) return;
+      turnOffSwitch();
+});
+    
