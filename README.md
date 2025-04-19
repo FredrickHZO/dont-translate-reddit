1e3cddc5f1211cfb824d9278a6c6b7544cc526da
unknown <federico.armagno97@gmail.com>
2025-04-19 20:13:58 +0200

    Update repo info

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dont-translate-reddit
 Simple browser add-on that removes Reddit automatic translation
+
+The chrome version uses the Manifest version 3, while the Firefox one uses the version 2 due to
+compatibility issues.
--- popup.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.getElementById('toggle');
-
-chrome.storage.local.get('enabled', ({ enabled }) => {
-    checkbox.checked = enabled ?? true;
-});
-
-checkbox.addEventListener('change', () => {
-    chrome.storage.local.set({ enabled: checkbox.checked });
-});
